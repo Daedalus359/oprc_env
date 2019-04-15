5d11914203d655607202bcc320e8c79b48d9b3ea
= <=>
2019-04-15 10:55:51 -0700

    svg generation

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
+
 module Main where
 
+--oprc imports
 import Env
 import MoveCosts
 import qualified SampleVals as SV
 
-main :: IO ()
-main = do
-  putStrLn "Starting Environment"
-  putStrLn $ show SV.pat
+--diagrams imports
+import Diagrams.Prelude
+import qualified Diagrams.Backend.SVG.CmdLine as BE
+
+myCircle :: Diagram BE.B
+myCircle = circle 1
+
+--main :: IO ()
+--main = do
+  --putStrLn "Starting Environment"
+  --putStrLn $ show SV.pat
+
+main = BE.mainWith myCircle

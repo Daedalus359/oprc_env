65321b6a0b8d7973918410a652e178d34d4d1a7d
kevin <=>
2019-04-15 15:23:33 -0700

    changed main to focus on example values again, removed comments in cabal file

--- exe/Main.hs
+++ exe/Main.hs
@@ -10,46 +10,15 @@ import MoveCosts
 import qualified SampleVals as SV
 
 --diagrams imports
---import qualified Diagrams.Prelude as D
---import qualified Diagrams.Backend.SVG.CmdLine as BE
+import qualified Diagrams.Prelude as D
+import qualified Diagrams.Backend.SVG.CmdLine as BE
 
---
-import Control.Monad.Trans (liftIO)
-import Graphics.UI.Gtk
-import Diagrams.Backend.Cairo
-import Diagrams.Backend.Cairo.Gtk
-import Diagrams.Prelude
-
---myCircle :: D.Diagram BE.B
---myCircle = D.circle 1
-
---main :: IO ()
---main = do
-  --putStrLn "Starting Environment"
-  --putStrLn $ show SV.pat
-
---main = BE.mainWith myCircle
+myCircle :: D.Diagram BE.B
+myCircle = D.circle 1
 
 main :: IO ()
 main = do
-  initGUI
-  window <- windowNew
-  canvas <- drawingAreaNew
-  canvas `on` sizeRequest $ return (Requisition 256 256)
-  set window [ containerBorderWidth := 10,
-               containerChild := canvas ]
-  canvas `on` exposeEvent $ renderFigure
-  onDestroy window mainQuit
-  widgetShowAll window
-  mainGUI
+  putStrLn "Starting Environment"
+  putStrLn $ show SV.pat
 
-
-renderFigure :: EventM EExpose Bool
-renderFigure = do
-   win <- eventWindow
-   liftIO $ renderToGtk win $ toGtkCoords figure
-   return True
-
-
-figure :: Diagram Cairo R2
-figure =  unitCircle # scaleX 0.5 # rotateBy (1/6) # scale 50 # fc red  
+--main = BE.mainWith myCircle

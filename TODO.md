583fcafb3fbbc2a6212a495584d435eba11c1cd4
= <=>
2019-07-01 13:57:10 -0700

    parsers for several types of actions

--- TODO.md
+++ TODO.md
@@ -1,5 +1,8 @@
-* Write a parser for Drone that turns a digit into a Drone
+To-Do List:
 * write a parser for NextActions that expects input like "[(1, Hover), (2, MoveIntercardinal NE)]"
 * finish manualControl function for interactive control of an environment
 * delete terminalNextActions function (should be renedered obsolete by parsers)
 * finish updateState function so that everything seems to work properly under manualControl in tests
+
+Completed Items:
+* Write a parser for Drone that turns a digit into a Drone (7/1/2019)
--- src/ParseOPRC.hs
+++ src/ParseOPRC.hs
@@ -2,8 +2,64 @@ module ParseOPRC where
 
 --datatypes to be parsed
 import Drone
+import Env
 
 --parser tools
 import Text.Trifecta
 
 doesItCompile = "?"
+
+--parses an integer, packs that int into a Drone
+parseDrone :: Parser Drone
+parseDrone = do
+  id <- natural --natural parses a positive integer
+  return (DroneID id)
+
+parseCardinal :: Parser CardinalDir
+parseCardinal = do
+  dirStr <- some letter
+  case dirStr of
+    "North" -> return North
+    "South" -> return South
+    "East" -> return East
+    "West" -> return West
+    _ -> fail "Could not parse direction"
+
+parseIntercardinal :: Parser IntercardinalDir
+parseIntercardinal = do
+  dirStr <- some letter
+  case dirStr of
+    "NE" -> return NE
+    "SE" -> return SE
+    "NW" -> return NW
+    "SW" -> return SW
+    _ -> fail "Could not parse direction"
+
+--parser for vertical directions
+
+parseAction :: Parser Action
+parseAction = do
+  actionStr <- some letter
+  _ <- spaces
+  case actionStr of
+    "MoveCardinal" -> do
+      dir <- parseCardinal
+      return (MoveCardinal dir)
+    "MoveIntercardinal" -> do
+      dir <- parseIntercardinal
+      return (MoveIntercardinal dir)
+    --handle movevertical case
+    --handle hover case
+    _ -> fail "Could not parse action"
+
+--shows how the parsers work
+parseDemo :: IO ()
+parseDemo = do
+  let p parser string = print $ parseString parser mempty string
+  p parseDrone "1"
+  p parseCardinal "North"
+  p parseCardinal "Blech"
+  p parseIntercardinal "NE"
+  p parseIntercardinal "Blech"
+  p parseAction "MoveCardinal East"
+  p parseAction "MoveIntercardinal SW"

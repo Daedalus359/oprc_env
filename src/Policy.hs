cbffde699880f863c9636e732e5c4fbf39019f3c
= <=>
2019-07-15 13:26:18 -0700

    documented rationale for nextMove type change

--- src/Policy.hs
+++ src/Policy.hs
@@ -9,6 +9,9 @@ import Data.Maybe
 
 class Policy p where
   nextMove :: p -> WorldView -> (NextActions, p)
+  --p can encode how it will learn from the next WorldView it gets to see as a function of the most recent NextActions it sent
+    --the above is not necessary for an implementation of Policy, but is probably required to learn an effective policy in complex environments
+
 
 --how to expose a policy in the process of being learned? IO Policy? State?
 

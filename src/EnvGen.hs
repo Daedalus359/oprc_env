fb36a113aa653862265f83155bc5fdbe578969b2
= <=>
2019-08-19 11:33:31 -0700

    environment generation

---
+++ src/EnvGen.hs
@@ -0,0 +1,11 @@
+module EnvGen where
+
+import Env
+
+import System.Random
+
+class EnvGen e where
+  mkEnv :: e -> StdGen -> Environment
+
+--the double should be a valid probability, i.e. between 0 and 1
+data BernoulliGen = BernoulliGen Double

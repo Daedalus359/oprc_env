932be38c49761711505c4f6fa9a098018c0621e8
= <=>
2019-08-22 16:35:32 -0700

    more progress on random footprint generation

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@ To-Do List:
 * make a function that randomly generates a footprint based on x and y bounds
 * make a function that adds holes to a footprint
 * make a function to fill in each patch of the environment with a bernoulli chance of Far / Close
+* make a function that will generate a .env file from an environment
 * make a function that gives the probability of a patch type as a function of its neighbors
 * adapt the online density estimation algorithms from Wenhao Luo to use observation filtering with a Kernel to estimate quality of fit of various environment models, then do planning based on that info
 * benchmark and improve my code's performance so that I can run 500 random trials of env 7 really fast
--- src/EnvGen.hs
+++ src/EnvGen.hs
@@ -17,6 +17,18 @@ patchBernoulli threshold gen =
     else (Patch Close, gen2)
   where (val, gen2) = randomR (0, 1) gen
 
+randomFootprint :: StdGen -> Int -> Int -> Int -> Int -> Maybe Footprint
+randomFootprint gen xMin xMax yMin yMax = undefined
+  --check if bounds are compatible
+  --generate 4 boundary Positions by adding random y values to the two x bounds and vice versa
+  --compute the left frontier by stitching together
+
+frontier :: Position -> [XCoord] -> Position -> [XCoord] -> Position -> [Position]
+frontier atYMin bridge1 atXBound bridge2 atYMax = undefined
+  --make zip pairs with the corresponding y values for bridge1 and bridge2
+
+
+--run fmap (clamp lowerBound upperBound) on the result of this if you have bounds to respect
 bridge :: StdGen -> Int -> Position -> Position -> [XCoord]
 bridge gen varLimit p1@(Position x1 y1) p2@(Position x2 y2) = tail $ scanl (+) x1 deltas
 
@@ -24,7 +36,10 @@ bridge gen varLimit p1@(Position x1 y1) p2@(Position x2 y2) = tail $ scanl (+) x
 
     deltas = zipWith (+) differences xShifts
 
-    differences = (take rem $ repeat (avgDX + 1)) ++ (take (numVals - rem) $ repeat avgDX)--unshuffled - consider shuffling these for an improved approach
+    differences = (take rem $ repeat (avgDX + 1)) ++ (take (numVals - rem) $ repeat avgDX)
+      --unshuffled - consider shuffling these for an improved approach
+      --alternatively, being unshuffled might help move away from the x boundary quickly
+
     (avgDX, rem) = divMod (x2 - x1 - totalShift) numVals--average
 
     totalShift = sum xShifts

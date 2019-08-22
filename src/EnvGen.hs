77891475d9fdcd11a47ade28e2b3008a40262117
= <=>
2019-08-22 15:12:12 -0700

    work on environment border generation

--- src/EnvGen.hs
+++ src/EnvGen.hs
@@ -3,6 +3,7 @@ module EnvGen where
 import Env
 
 import System.Random
+import Data.Vector.Unboxed as UV
 
 class EnvGen e where
   mkEnv :: e -> StdGen -> Environment
@@ -17,16 +18,16 @@ patchBernoulli threshold gen =
     else (Patch Close, gen2)
   where (val, gen2) = randomR (0, 1) gen
 
---seededRow :: Int -> 
+bridge :: Position -> Position -> [XCoord]
+bridge p1@(Position x1 y1) p2@(Position x2 y2) = undefined
+  where
+    xDirFact = if (x1 < x2) then 1 else (-1)
+    yDirFact = if (y1 < y2) then 1 else (-1)
 
-convexFootprint :: Int -> Int -> Int -> StdGen -> Maybe Footprint
-convexFootprint width height numVertices gen =
-  if (numVertices < 3 || (width < 2) || (height  < 2))
-    then Nothing
-    else undefined
-
-makeConvex :: [Position] -> [Position]
-makeConvex positions = undefined
-  --find out which points are interior, filter those out
-  --see if the list is the same as before give that back if so
-  --otherwise add the difference in random points and run through again
+clamp :: Int -> Int -> Int -> Int
+clamp lowBound highBound num =
+  if num < lowBound
+    then lowBound
+    else if num > highBound
+            then highBound
+            else num

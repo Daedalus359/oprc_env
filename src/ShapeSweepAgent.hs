2ac7ff3772c02622ebf0d8333b14a0cce96f0d64
= <=>
2019-08-27 14:54:48 -0700

    data types for high sweep policy

--- src/ShapeSweepAgent.hs
+++ src/ShapeSweepAgent.hs
@@ -68,7 +68,13 @@ makeDirections (pos1 : rest@(pos2 : path)) =
     Nothing -> Nothing
     (Just action) -> fmap ((:) action) $ makeDirections rest
 
---policy should plan a path to the lowest ord - valued patch that has not been fully observed
+data SweepPhase = HighSweep | LowSweep
+
+data HighSweepPolicy = HighSweepPolicy
+  {
+    getPhase :: SweepPhase
+  , getDirs :: Directions
+  }
 
 --a second, "HighSweepPolicy" should probably use the same policy as LowSweep for its "phase 2" behavior, after the high environment has been explored and it has descended again
 --highsweepPolicy should use a smart function to query A* for paths to a filtered subset of nodes that make sense to visit (e.g rows 2, 5, 8, etc. with shape caveats)

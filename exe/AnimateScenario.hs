77da35964948d90ee0904687167d8326c7b6e585
= <=>
2019-10-24 13:31:25 -0700

    testing drone dropout with latest policy

--- exe/AnimateScenario.hs
+++ exe/AnimateScenario.hs
@@ -96,8 +96,9 @@ visualReplay2 replay@(ScenarioReplay ws _ _) = do
 
 main :: IO ()
 main =
-  --createReplayWithDropout nDrones <$> SV.fileNameScenarioDropout SV.albp nDrones filePath 20000 >>= visualReplay2
-  SV.fileNameScenarioWithOutput SV.hfsp 3 filePath 25000 >>= visualReplay
+
+  createReplayWithDropout nDrones <$> SV.fileNameScenarioDropout SV.hfsp nDrones filePath 20000 >>= visualReplay2
+  --SV.fileNameScenarioWithOutput SV.hfsp 3 filePath 25000 >>= visualReplay
   --SV.fileNameScenarioWithOutput SV.albp 6 filePath 20000 >>= visualReplay
   --SV.fileNameScenarioWithOutput SV.lkmstp 6 filePath 10000 >>= visualReplay
   --SV.fileNameScenarioWithOutput SV.lstp 1 filePath 100000 >>= visualReplay

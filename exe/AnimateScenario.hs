c90d8f26fc466b06e60c7f35f815ce4ea49b27ca
kevin <=>
2019-12-19 12:01:46 -0500

    new config for short wide end

--- exe/AnimateScenario.hs
+++ exe/AnimateScenario.hs
@@ -31,7 +31,7 @@ defaultFramerate = 60
 
 --turns a number of seconds elapsed into a number of sim timesteps elapsed
 speedupFactor :: Float
-speedupFactor = 2
+speedupFactor = 0.2
 
 --modify this to calculate the width of the scenario and pass a value based on that to drawReplay as the offset argument
 visualReplay :: Scenario p -> IO ()
@@ -105,7 +105,7 @@ main =
   --SV.fullScenarioWithOutput SV.kmp 6 8 100000 >>= visualReplay
   --SV.fullScenarioWithOutput (return $ const SV.lsPolicy) 6 7 100000 >>= visualReplay
   --SV.fullScenarioWithOutput SV.hfsp 1 4 110 >>= visualReplay
-  SV.fullScenarioWithOutput (return $ const $ LowSweepPolicy []) 1 4 110 >>= visualReplay
+  SV.fullScenarioWithOutput (return $ const $ LowSweepPolicy []) 1 11 110 >>= visualReplay
   where
     --filePath = "./test/environments/generated/2019-10-23 20:24:12.230291522 UTC/testMixed3.env"
     filePath = "./test/environments/generated/clumpedNE=0.25T=0.3.env"

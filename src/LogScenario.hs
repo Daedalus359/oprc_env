45651bcb97d7237aa149295767762b02b0332ab4
= <=>
2020-01-24 10:14:33 -0800

    CSV stuff

--- src/LogScenario.hs
+++ src/LogScenario.hs
@@ -1,5 +1,12 @@
 module LogScenario where
 
+{-
+Basically, this makes a variant of the scenario management code (i.e. runScenario) that logs information about the WorldState at each time step
+Then, it transforms the minimal information associated with each time step into a "row" that includes some derived quantities
+-}
+
 import Scenario
+
+
 
 testMe = "."
